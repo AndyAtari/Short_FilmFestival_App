7e9540caf70033cff9fcbf27339fc610eec0b224
Andrew Alteri <andrewsalteri@gmail.com>
2021-01-23 15:40:00 -0600

    Made frontend directory and font styling.

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Short Film Festival</h1>
+    <h2>Short Film Showcase</h2>
+    <h3>My movies</h3>
+    </>
+  );
+}
+
+export default App;
