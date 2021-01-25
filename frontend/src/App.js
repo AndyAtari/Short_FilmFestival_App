c94fb47b0a87bc76765adb54ceab56099263a2a6
Andrew Alteri <andrewsalteri@gmail.com>
2021-01-25 12:29:49 -0600

    Imported BrowserRouter and made component & container directories.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <h1>Short Film Festival</h1>
-    <h2>Short Film Showcase</h2>
-    <h3>My movies</h3>
-    </>
+    <div className="app-container">
+      <Router>
+        <h1>Short Film Festival</h1>
+        <h2>Short Film Showcase</h2>
+        <h3>My movies</h3>
+        <Switch></Switch>
+      </Router>
+    </div>
   );
 }
 
