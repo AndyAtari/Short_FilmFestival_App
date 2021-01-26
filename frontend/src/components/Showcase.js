828069e7998e4580b4d073249a8fbcef3a99c200
Andrew Alteri <andrewsalteri@gmail.com>
2021-01-26 16:21:27 -0600

    Built componentDidMount to fetch data and render data in Showcase.js

--- frontend/src/components/Showcase.js
+++ frontend/src/components/Showcase.js
@@ -1,14 +1,33 @@
-const Showcase = () => {
-  return (
-    <div className="showcase">
-      <h1>Showcase</h1>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-    </div>
-  );
-};
+// const Showcase = (props) => {
+//   return (
+//     <div className="showcase">
+//       <h1>Showcase</h1>
+//       <div>
+//         <img src={props.videos}></img>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Showcase;
+
+import React from "react";
+
+class Showcase extends React.Component {
+  videoShowcase = () => {
+    return this.props.videos.map((mov) => (
+      <img key={mov.id} src={mov.url} alt={mov.id} />
+    ));
+  };
+
+  render() {
+    return (
+      <div className="showcase">
+        <h1>Showcase</h1>
+        {this.videoShowcase()}
+      </div>
+    );
+  }
+}
 
 export default Showcase;
--- frontend/src/containers/HomeContainer.js
+++ frontend/src/containers/HomeContainer.js
@@ -4,11 +4,23 @@ import Showcase from "../components/Showcase";
 import TopMovies from "../components/TopMovies";
 
 class HomeContainer extends Component {
+  state = {
+    videos: [],
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/movies")
+      .then((resp) => resp.json())
+      .then((videos) => {
+        this.setState({ videos });
+      });
+  }
+
   render() {
     return (
       <div className="home">
         <Watchlist />
-        <Showcase />
+        <Showcase videos={this.state.videos} />
         <TopMovies />
       </div>
     );
