b7d29d7157d545f5cf74d34d3e2a458b7db26d97
Andrew Alteri <andrewsalteri@gmail.com>
2021-01-29 14:43:56 -0600

    Reset database and added name column to movie table.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_003339) do
+ActiveRecord::Schema.define(version: 2021_01_29_193921) do
 
   create_table "movies", force: :cascade do |t|
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
 end
--- frontend/src/components/Showcase.js
+++ frontend/src/components/Showcase.js
@@ -3,6 +3,7 @@ import React from "react";
 const Showcase = (props) => {
   return (
     <div>
+      <p>{props.mov.name}</p>
       <img key={props.id} src={props.mov.url} alt={props.id} />
     </div>
   );
