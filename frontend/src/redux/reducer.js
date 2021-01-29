df2c24519fab496f26b9a666506706c656630f74
Andrew Alteri <andrewsalteri@gmail.com>
2021-01-29 15:46:53 -0600

    Built simple reducer. WIP moving logic to Redux.

---
+++ frontend/src/redux/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  videos: [],
+  watchList: [],
+  offset: 0,
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return { ...state };
+  }
+};
