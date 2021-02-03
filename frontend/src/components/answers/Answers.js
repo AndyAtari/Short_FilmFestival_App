4c071ebb0210bb3ba9cd8959da02056e32b8f9d3
Andrew Alteri <andrewsalteri@gmail.com>
2021-02-03 17:11:40 -0600

    Built Answer component that renders an answer list based on association with question.

---
+++ frontend/src/components/answers/Answers.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Answer from "./Answer";
+
+class Answers extends Component {
+  render() {
+    const { answers, questionId, deleteAnswer } = this.props;
+    const associatedAnswers = answers.filter(
+      (answer) => answer.questionId === questionId
+    );
+
+    const answerList = associatedAnswers.map((answer, index) => {
+      return <Answer key={index} answer={answer} deleteAnswer={deleteAnswer} />;
+    });
+    return (
+      <div>
+        <ul>{answerList}</ul>
+      </div>
+    );
+  }
+}
+
+export default Answers;
