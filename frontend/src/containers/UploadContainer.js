2315af8fdeeda9a5f74c53decff0eeed8b6f6a42
Andrew Alteri <andrewsalteri@gmail.com>
2021-02-04 00:37:54 -0600

    WIP, built uploadContainer and moved dipatch to actionCreator.

--- frontend/src/containers/UploadContainer.js
+++ frontend/src/containers/UploadContainer.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import UploadCard from "../components/uploads/UploadCard";
+import { uploadVideo } from "../redux/actionCreator";
 
 class UploadContainer extends Component {
   render() {
+    const { uploads } = this.props;
     return (
       <div>
         <h1>Submit Your Animated Short Film</h1>
+        <div>
+          {uploads.map((upload) => (
+            <UploadCard upload={upload} />
+          ))}
+        </div>
       </div>
     );
   }
 }
 
-export default UploadContainer;
+const mapStateToProps = (state) => {
+  return {
+    uploads: state.uploads,
+  };
+};
+
+export default connect(mapStateToProps, { uploadVideo })(UploadContainer);
--- frontend/src/redux/actionCreator.js
+++ frontend/src/redux/actionCreator.js
@@ -1,3 +1,10 @@
+export const uploadVideo = (uploads) => {
+  return {
+    type: "UPLOAD_VIDEO",
+    uploads: Object.assign({}, uploads),
+  };
+};
+
 export const getVideos = () => {
   return (dispatch) => {
     fetch("http://localhost:3000/movies")
