d051524eab248cbfae5513f309eb76a4397dd29c
Andrew Alteri <andrewsalteri@gmail.com>
2021-02-04 10:01:10 -0600

    Built Upload Form, WIP.

--- frontend/src/components/UploadForm.js
+++ frontend/src/components/UploadForm.js
@@ -1,3 +1,47 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { uploadVideo } from "../redux/actionCreator";
+
+class UploadForm extends Component {
+  state = {
+    title: "",
+    link: "",
+  };
+
+  handleOnChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleOnSubmit}>
+          <input
+            type="text"
+            placeholder="Title of Short Film"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleOnChange}
+          />
+          <input
+            type="text"
+            placeholder="Embeded Url of Film"
+            name="link"
+            value={this.state.link}
+            onChange={this.handleOnChange}
+          />
+          <button type="submit">Submit For Consideration</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default connect(null, { uploadVideo })(UploadForm);
--- frontend/src/components/uploads/UploadCard.js
+++ frontend/src/components/uploads/UploadCard.js
@@ -1,1 +1,5 @@
+import React from "react";
 
+const UploadCard = ({ upload }) => <div></div>;
+
+export default UploadCard;
