be1855542f152916faa3ae0461b08a541381ef6e
Andrew Alteri <andrewsalteri@gmail.com>
2021-02-05 15:46:19 -0600

    test commit

--- frontend/src/redux/reducer.js
+++ frontend/src/redux/reducer.js
@@ -12,19 +12,23 @@ export const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "SET_VIDEOS":
       return { ...state, videos: action.payload.videos };
+
     case "ADD_QUESTION":
       const question = { text: action.text, id: cuidFn() };
       return {
         ...state,
         questions: [...state.questions, question],
       };
+
     case "SET_QUESTIONS":
       return { ...state, questions: action.payload.questions };
+
     case "DELETE_QUESTION":
       const questions = state.questions.filter(
         (question) => question.id !== action.id
       );
       return { ...state, questions };
+
     case "ADD_ANSWER":
       const answer = {
         text: action.answer.text,
@@ -32,9 +36,14 @@ export const reducer = (state = initialState, action) => {
         id: cuidFn(),
       };
       return { ...state, answers: [...state.answers, answer] };
+
+    case "SET_ANSWERS":
+      return { ...state, answers: action.payload.answers };
+
     case "DELETE_ANSWER":
       const answers = state.answers.filter((answer) => answer.id !== action.id);
       return { ...state, answers };
+
     case "UPLOAD_VIDEO":
       const newVideo = { text: action.text };
       return {
